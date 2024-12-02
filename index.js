a7be3b8cff10e4905a1c0ccf99913b5e528d80f3
abdulsamii786 <abdulsami0324@gmail.com>
2024-12-03 02:50:00 +0500

    updated code

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 import express from "express";
 import dotenv from "dotenv";
+import albumsRoute from "./routes/albumsRoute.js";
+import commentsRoute from "./routes/usersRoute.js";
+import photosRoute from "./routes/photosRoute.js";
+import postsRoute from "./routes/postsRoute.js";
+import todosRoute from "./routes/todosRoute.js";
+import usersRoute from "./routes/usersRoute.js";
 const app = express();
 
 dotenv.config();
 
-const PORT = process.env.PORT;
+app.get("/", (req, res) => {
+  res.status(200).send("Welcome To My API");
+});
+
+app.use("/albums", albumsRoute);
+app.use("/comments", commentsRoute);
+app.use("/photos", photosRoute);
+app.use("/posts", postsRoute);
+app.use("/todos", todosRoute);
+app.use("/users", usersRoute);
+
+app.get("*", (req, res) => {
+  res.status(404).send("Page Not Found");
+});
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
-  console.log("server started");
+  console.log(`server is running on port${PORT}`);
 });
